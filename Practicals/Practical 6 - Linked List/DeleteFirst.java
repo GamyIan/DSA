3f06c450b3664fbaaa2d558a0e4180d1d5cf530b
Ian Reddy <73981521+GamyIan@users.noreply.github.com>
2026-02-06 11:48:23 +0000

    Added deletion of Node from Linked List

---
+++ Practicals/Practical 6 - Linked List/DeleteFirst.java
@@ -0,0 +1,59 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data=data;
+    }
+
+    Node(int data,Node next){
+        this.data=data;
+        this.next = next;
+    }
+
+    void setData(int data){
+        this.data = data;
+    }
+
+    int getData(){
+        return this.data;
+    }
+
+    void setNext(Node next){
+        this.next = next;
+    }
+
+    Node getNext(){
+        return this.next;
+    }
+}
+
+class DeleteFirst {
+    public static void main(String[] args){
+        Node first = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+
+        first.setNext(second);
+        second.setNext(third);
+
+        System.out.println("Traversing the Original Linked List:");
+
+        Node curr = first;
+        while(curr!=null){
+            System.out.println(curr.getData());
+            curr = curr.getNext();
+        }
+
+        // Deleting first Node 
+        first.setNext(null);
+
+        System.out.println("Traversing the new LinkedList without first:");
+        curr = second; // start traversal from second
+        while(curr!=null){
+            System.out.println(curr.getData());
+            curr = curr.getNext();
+        }
+        
+    }
+}
